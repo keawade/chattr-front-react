289bf516dca752be511f09f0cd8770340598dc9e
Keith Wade <keawade@gmail.com>
2016-06-10 23:59:06 -0500

    Boiler plate

--- src/index.js
+++ src/index.js
@@ -1,44 +1,16 @@
-import _ from 'lodash'
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import YTSearch from 'youtube-api-search'
-import SearchBar from './components/search-bar'
-import VideoList from './components/video-list'
-import VideoDetail from './components/video-detail'
-
-const API_KEY = ''
 
 class App extends Component {
   constructor (props) {
     super(props)
-
-    this.state = {
-      videos: [],
-      selectedVideo: null
-    }
-
-    this.videoSearch('hearthstone')
-  }
-
-  videoSearch (term) {
-    YTSearch({key: API_KEY, term: term}, videos => {
-      this.setState({
-        videos: videos,
-        selectedVideo: videos[0]
-      })
-    })
+    this.state = {}
   }
 
   render () {
-    const videoSearch = _.debounce(term => { this.videoSearch(term) }, 300)
     return (
       <div>
-        <SearchBar onSearchTermChange={videoSearch} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList
-          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
-          videos={this.state.videos}
-        />
+        <h1>This is a test.</h1>
       </div>
     )
   }
